7b6a918a36cbf3f0f853a44eb8995ee7c9706b68
rithul17 <rithul1905@gmail.com>
2025-03-13 19:37:46 +0530

    change

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, Jenkins\n";
-  cout << "i am pes2ug22cs449 king \n"
+  cout << "i am pes2ug22cs449 king \n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
