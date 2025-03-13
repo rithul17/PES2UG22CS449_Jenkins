b643f1644ed8ac0a0058bf9752ebbc84fb0f5036
rithul17 <rithul1905@gmail.com>
2025-03-13 20:17:20 +0530

    done hopefully

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Jenkins Pipeline!" << std::endl;
+    std::cout << "This is a test C++ program for PES2UG22CS449" << std::endl;
+    return 0;
+}
